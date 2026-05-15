76edc1cf3f875c60a28730800e017ebcb66ca0f2
radiantwave28-lang <radiantwave28@gmail.com>
2026-05-15 21:00:17 +0200

    Add RootLayout component with metadata

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'NijaSwap - Buy & Sell in Nigeria',
+  description: 'Nigeria Premier Marketplace by RadiantWaves Media',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
